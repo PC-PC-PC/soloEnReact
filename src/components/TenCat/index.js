2dba6011652c185acee0d96c7a6f90fe4b904895
Yuchan45 <yu.nakasone@gmail.com>
2019-11-20 21:52:08 -0300

    cambios en los botones de tendencias

--- src/components/TenCat/index.js
+++ src/components/TenCat/index.js
@@ -58,182 +58,26 @@ class TenCat extends Component {
         })
 
       })
-
-      /*fetch ('http://localhost:4000/MLHuergo/CatTend/delete', { //FUNCIÓN PARA BORRAR TODO 
-
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-        }
-  
-      })*/
-/*     fetch('http://localhost:4000/MLHuergo/CatTend/add', { //GUARDAR DE A UNO (PRIMERO GUARDO Y DESPUÉS BORRO)
-                        
-      method: 'POST',
-      body: JSON.stringify(
-        {
-          "_name": "Accesorios para VehículosMax",
-          "_day": "Irrelevante",
-          "_cant": 0         
-        },
-        {
-          "_name": "Alimentos y BebidasMax",
-          "_day": "Irrelevante",
-          "_cant": 0
-        },
-        {
-          "_name": "Animales y MascotasMax",
-          "_day": "Irrelevante",
-          "_cant": 0     
-        },
-        {
-          "_name": "Antigüedades y ColeccionesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Arte, Librería y MerceríaMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Autos, Motos y OtrosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "BebésMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Belleza y Cuidado PersonalMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Cámaras y AccesoriosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Celulares y TeléfonosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "ComputaciónMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Consolas y VideojuegosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Deportes y FitnessMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Electrodomésticos y Aires Ac.Max",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Electrónica, Audio y VideoMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Entradas para EventosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Herramientas y ConstrucciónMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Hogar, Muebles y JardínMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Industrias y OficinasMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "InmueblesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Instrumentos MusicalesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Joyas y RelojesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Juegos y JuguetesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Libros, Revistas y ComicsMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Música, Películas y SeriesMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Ropa y AccesoriosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Salud y Equipamiento MédicoMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "ServiciosMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Souvenirs, Cotillón y FiestasMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        },
-        {
-          "_name": "Otras categoríasMax",
-          "_day": "Irrelevante",
-          "_cant": 0    
-        }
-      ),
-      headers:{
-          'Content-Type': 'application/json',
-      }
-
-     })
- */      .then(function (res) { 
+    
+       .then(function (res) { 
         console.log(res);
         respuesta = JSON.stringify(res)
         console.log(respuesta)
       })
 
+  }
+  ultimo_dia(){
+
   } 
+  ultima_semana(){
+
+  }
+  ultimo_mes(){
+
+  }
+  ultimo_año(){
+
+  }
   render() {
 
     return (
@@ -247,27 +91,19 @@ class TenCat extends Component {
 
           <div algo = "alinear">
             <div class = "inner">
-              <button class =  "dia" >
-                Último día
-              </button>
+              <button onClick = {this.ultimo_dia.bind(this)} type = "button" class = "btn btn-primary" > Ultimo dia </button>
             </div>
   
             <div class = "inner">
-              <button class =  "semana" >
-                Última semana
-              </button>
+              <button onClick = {this.ultima_semana.bind(this)} type = "button" class = "btn btn-dark" > Ultima semana </button>
             </div>
 
             <div class = "inner">
-              <button class =  "mes" >
-                Último mes
-              </button>
+              <button onClick = {this.ultimo_mes.bind(this)} type = "button" class = "btn btn-success" > Ultimo mes </button>
             </div>
 
             <div class = "inner">
-              <button class =  "año" >
-                Último año
-              </button>
+              <button onClick = {this.ultimo_año.bind(this)} type = "button" class = "btn btn-danger" > Ultimo año </button>
             </div>
         
           </div>
